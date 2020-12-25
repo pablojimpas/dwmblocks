0aa310b57d5bbaee38b18f4c9bc9a4895e4eea1c
pablojimpas <pablo@jimpas.me>
2020-12-25 23:46:02 +0100

    Some personal preferences

--- config.h
+++ config.h
@@ -6,6 +6,7 @@ static const Block blocks[] = {
 	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
 	{"",	"news",		0,	6},
+	{"",	"mailbox",	180,	12},
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -13,17 +14,16 @@ static const Block blocks[] = {
 	/* {"",	"price xmr \"Monero\" 🔒",			9000,	24}, */
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
-	/* {"",	"torrent",	20,	7}, */
+	{"",	"torrent",	20,	7},
+	/* {"",	"moonphase",	18000,	17}, */
+	/* {"",	"forecast",	18000,	5}, */
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
-	{"",	"mailbox",	180,	12},
 	{"",	"nettraf",	1,	16},
+	{"",	"internet",	5,	4},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
 	{"",	"help-icon",	0,	15},
 };
 
